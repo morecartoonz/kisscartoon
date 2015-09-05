a719e333a69af8e1affc156ceb579598a7a3ad38
Jan Walther <jan.f.walther@gmail.com>
2015-09-05 13:29:37 +0200

    Switched to Python and bypassed Cloudflare

--- README.md
+++ README.md
@@ -1,26 +1,35 @@
 # Kisscartoon Batch Downloader
 
-Get's the direct Links to Video Files from Kisscartoon
+Get's and downloads Videos from Kisscartoon
 
 # Usage
 
 
-```./kisscartoon.sh <cartoon page> ```
+```./python kisscartoon.py <cartoon page> ```
+
+If you don't want extra Information just the download add -q or --quiet
+```./python kisscartoon.py -q <cartoon page> ```
+
+If you want to handle the downloading yourself use --only-links or -l to just print the links
+```./python kisscartoon.py -l <cartoon page> ```
+
+If you want the links written to a File use --save-links or -s to save them to links.txt
+```./python kisscartoon.py -s <cartoon page> ```
+
+Those Operators are freely combinable
 
 
-```./kisscartoon.sh http://kisscartoon.me/Cartoon/Aqua-Teen-Hunger-Force-Season-10```
-
-Use ```-c``` to copy the links to your Clipboard. Needs xclip installed.
-
-```./kisscartoon.sh <cartoon page> -c```
+```./python kisscartoon.py -q -s http://kisscartoon.me/Cartoon/Aqua-Teen-Hunger-Force-Season-10```
 
 # Requirements
 
-Python
+Python :
+# cfscrape
+# BeautifulSoup4 ( bs4 )
+# pySmartDL
 
-Spidermonkey
+All of the dependencies are available via pip and easy_install
 
-xclip ( optional )
 
  
 
---
+++ kisscartoon.py
@@ -0,0 +1,92 @@
+__author__ = 'Jan'
+import base64
+
+from bs4 import BeautifulSoup
+import cfscrape
+from pySmartDL import SmartDL
+import os.path
+import sys
+import argparse
+
+
+def get_episodes(url):
+    html = scraper.get(url).content
+    soup = BeautifulSoup(html, "lxml")
+
+    table = soup.find("table", "listing")
+
+    tablesoup = BeautifulSoup(str(table), "lxml")
+
+    links = []
+
+    for link in tablesoup.findAll('a'):
+        links.append("%s%s" % ("http://kisscartoon.me", link.get('href')))
+    return links
+
+
+def get_episode(url, quiet=False):
+    html = scraper.get(url).content
+    soup = BeautifulSoup(html, "lxml")
+    link = soup.find("select", {"id": "selectQuality"})
+
+    valuesoup = BeautifulSoup(str(link), "lxml")
+    dlinks = []
+    quali = []
+    for src in valuesoup.findAll('option'):
+        quali.append(src.text)
+        dlinks.append(base64.b64decode(src.get('value')))
+    if not quiet:
+        print("Quality : " + quali[0])
+    return dlinks
+
+
+def download(url, name, quiet = False):
+    path = ".\\%s" % name
+    if os.path.isfile(path):
+        if not quiet:
+            print "Skipping %s" % name
+        return
+    else:
+        obj = SmartDL(url, ".\\%s" % name)
+        obj.start()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("show")
+parser.add_argument("-l","--only-links", help="only print's the links. doesn't download the files.", action="store_true")
+parser.add_argument("-s","--save-links", help="saves the links to links.txt", action="store_true")
+parser.add_argument("-q","--quiet", help="shut's up", action="store_true")
+args = parser.parse_args()
+
+
+scraper = cfscrape.create_scraper()
+
+show = args.show
+
+showname = show.split("/")[-1]
+if not args.quiet:
+    print("Serie : %s" % showname)
+
+links = []
+
+for episode in list(reversed(get_episodes(show))):
+    episodename = episode.split("/")[-1].split("?")[0] + ".mp4"
+    if not args.quiet:
+        print "Episode : %s" % episodename
+
+    link = get_episode(episode, args.quiet)[0]
+
+    if args.only_links:
+        print(link)
+    else:
+        download(link, "%s_%s" % (showname, episodename), args.quiet)
+
+    if args.save_links:
+        links.append(link)
+    print("\n")
+if args.save_links:
+    with open('links.txt', 'a') as file:
+        for link in links:
+            file.write("%s" % (link) )
+
+
